92be1b1ced584046dc72ea446672f1d4e0df44f4
HeadInoueTakuma <inoue.takuma@pro.headwaters.co.jp>
2022-12-20 22:05:55 +0900

    モックを作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,45 @@
-import React from "react";
-import "./style.css"
+import React, { useState } from "react";
+import "./style.css";
 
-const App = () => {
-  return "aa";
-}
+export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState(["test1", "test2"]);
+  const [completeTodos, setCompleteTodos] = useState(["test3"]);
 
-export default App;
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <div>
+          <p className="title">完了のTsODO</p>
+          <ul>
+            {completeTodos.map((todo) => {
+              return (
+                <div key={todo} className="list-row">
+                  <li>{todo}</li>
+                  <button>戻す</button>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
