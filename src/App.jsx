b5f5ffac1ad7c9234db834fa14762c5d2666bc3e
HeadInoueTakuma <inoue.takuma@pro.headwaters.co.jp>
2022-12-22 20:32:14 +0900

    reactでtodoリストの作成

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,47 @@ export const App = () => {
   // インプットエリアに文字が入力されるたびに値を取得する
   const onChangeTodoText = (event) => setTodoText(event.target.value);
 
-
   const onClickAdd = () => {
     // インプットエリアが空文字だったら処理を終了させる
-    if (todoText === '') return;
-    // incompleteTodos変数？にtodoText（入力された値を追加）を追加した配列を返す
+    if (todoText === "") return;
+    // incompleteTodosにtodoText（入力された値を追加）を追加した配列を返す
     const newTodos = [...incompleteTodos, todoText];
-    // newTodosの配列を確認
-    // console.log(newTodos);
     setIncompleteTodos(newTodos);
     // 追加ボタンが押された時にインプットエリアを空文字にする
-    setTodoText('');
+    setTodoText("");
   };
+
+  const onClickComplete = (index) => {
+    // 完了が押されたタスクを未完了のtodoから削除する
+    // 未完了のtodoリストをコピーする
+    const newIncompleteTodos = [...incompleteTodos];
+    // コピーしたリストからindexでとってきたキーの値を削除する
+    newIncompleteTodos.splice(index, 1);
+    // stateの状態をnewIncompleteTodosに変える
+    setIncompleteTodos(newIncompleteTodos);
+
+    // 既存の完了のtodoリストを複製
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+  };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    // indexでとってきたキーから、1つの要素を削除する
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
+  const onClickBack = (index) => {
+
+    // 完了リストの配列を複製している
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+    
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+    setIncompleteTodos(newIncompleteTodos);
+  }
 
   return (
     <>
@@ -40,12 +69,12 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
@@ -55,11 +84,11 @@ export const App = () => {
         <div>
           <p className="title">完了のTODO</p>
           <ul>
-            {completeTodos.map((todo) => {
+            {completeTodos.map((todo, index) => {
               return (
                 <div key={todo} className="list-row">
                   <li>{todo}</li>
-                  <button>戻す</button>
+                  <button onClick={() => onClickBack(index)}>戻す</button>
                 </div>
               );
             })}
